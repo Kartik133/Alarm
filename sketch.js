9193d35d70448d81e7d8a1ad0b89eb7a9a88414e
Kartik133 <75470483+Kartik133@users.noreply.github.com>
2021-10-25 19:36:28 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -67,9 +67,9 @@ function draw() {
 
     push();
      textSize(50);
-     text("Hour",width/2-100,height/2-110);
-     text("Minute",width/2-100,height/2-10);
-     text("Second",width/2-100,height/2+90);
+     text("HOUR",width/2-100,height/2-110);
+     text("MINUTE",width/2-100,height/2-10);
+     text("SECOND",width/2-100,height/2+90);
     pop();
   }
 
